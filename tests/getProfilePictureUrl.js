c22fd13c2e063177d06b0c7759379a42ea3d9e2c
Dominik Ferber <dominik.ferber@gmail.com>
2015-04-01 14:38:27 +0200

    add basic version of test.

--- tests/getProfilePictureUrl.js
+++ tests/getProfilePictureUrl.js
@@ -1,7 +1,39 @@
 'use strict';
 
-describe('A suite for just the server', function(){
-	it('runs server side', function(){
-		expect(2).to.equal(2);
+describe('Graph API', function () {
+	describe('Methods', function() {
+		describe('getProfilePictureUrl', function () {
+
+			var facebook;
+
+			before(function () {
+				facebook = new GraphAPI();
+
+				// mock _call
+				facebook._call = function (method, edge, params) {
+					var edgePath = this._getEdgePath(edge);
+
+					return new HTTPResponse(this.__response);
+				}
+			});
+
+			it('returns a string', function() {
+				var validUrl = 'https://www.facebook.com/profile-picture.jpg';
+
+				facebook.__response = {
+					statusCode: 200,
+					data: {
+						data: {
+							url: validUrl
+						}
+					},
+					headers: {
+
+					}
+				};
+
+				expect(facebook.getProfilePictureUrl()).to.equal(validUrl);
+			});
+		});
 	});
 });
